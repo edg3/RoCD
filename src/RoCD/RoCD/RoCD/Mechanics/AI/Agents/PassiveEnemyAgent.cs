f050067301337bcfca8febe9b5ac69614dca090c
Ernest Loveland <loveland.ernest@gmail.com>
2015-08-02 11:35:45 -0700

    Fixed logging for PassiveEnemyAgent

--- src/RoCD/RoCD/RoCD/Mechanics/AI/Agents/PassiveEnemyAgent.cs
+++ src/RoCD/RoCD/RoCD/Mechanics/AI/Agents/PassiveEnemyAgent.cs
@@ -19,7 +19,7 @@ namespace RoCD.Mechanics.AI.Agents
                 _map.MoveActor(_agent_on, _map.AStarTo(_agent_on, _target));
                 if (Math.Sqrt(Math.Pow(_agent_on.X - _target.X, 2) + Math.Pow(_agent_on.Y - _target.Y, 2)) < 1.5)
                 {
-                    CombatLog.Log("PassiveEnemyAgent: agressive, close enough to attack player.");
+                    CombatLog.GameLog("PassiveEnemyAgent: agressive, close enough to attack player.");
                     //Attack target!
                     (_agent_on as Creature).meleeAttack(_target as Creature);
                 }
@@ -29,7 +29,7 @@ namespace RoCD.Mechanics.AI.Agents
                 //check distance, if too far remove:
                 if (Math.Sqrt(Math.Pow(_agent_on.X - _target.X, 2) + Math.Pow(_agent_on.Y - _target.Y, 2)) > 50)
                 {
-                    CombatLog.Log("PassiveEnemyAgent: decided too far from player, time to die.");
+                    CombatLog.GameLog("PassiveEnemyAgent: decided too far from player, time to die.");
                     (_agent_on as Creature).set(Creature.CURRHP, 0);
                     return;
                 }
@@ -37,7 +37,7 @@ namespace RoCD.Mechanics.AI.Agents
                 //Chance to roam
                 if (RoCDRndm.Next(100) < ChanceToRoam)
                 {
-                    CombatLog.Log("PassiveEnemyAgent: I will roam.");
+                    CombatLog.GameLog("PassiveEnemyAgent: I will roam.");
                     _map.MoveActor(_agent_on, Helpers.Tiles.Map.Direction.Random);
                 }
             }
